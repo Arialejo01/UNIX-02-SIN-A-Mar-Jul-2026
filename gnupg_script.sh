a170ff1d12e351eb078235d44fca006ac98f00b3
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-20 18:02:14 +0000

    libs and keys generation

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,3 +1,4 @@
 uname -a #brinda infromacion del ordenador en el que se opera el codigo
 which gpg # ubicacion del binario del gpg
 gpg --version #da la versioin e informacion adicional
+gpg --full-generate-key #genero llave publica y privada
