28997369efa8a4b6eb7c7213038ae2817a6b8021
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-20 17:36:08 +0000

    version

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,2 +1,3 @@
 uname -a #brinda infromacion del ordenador en el que se opera el codigo
-which gpg # ubicacoin del binario del gpg
+which gpg # ubicacion del binario del gpg
+gpg --version #da la versioin e informacion adicional
