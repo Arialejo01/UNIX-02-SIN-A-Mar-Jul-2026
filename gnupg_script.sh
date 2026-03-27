03336e1d8c6b245c562ee8e7f4ce1b6c13068975
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-27 01:34:06 +0000

    class 20-03-26

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,4 +1,7 @@
-uname -a #daba información del ordenador
-which gpg # ubicacióin del binario de gpg
-gpg --version # me da la version e informacion adicional
-gpg --full-key-generate #genero la llave publica y privada
+uname -a #brinda infromacion del ordenador en el que se opera el codigo
+which gpg # ubicacion del binario del gpg
+gpg --version #da la versioin e informacion adicional
+gpg --full-generate-key #genero llave publica y privada
+gpg --list-keys #Muestra todas las claves GPG públicas de su llavero local, incluidos los ID de usuario, los ID de clave y las huellas digitales
+gpg --armor --export #Exporta una clave pública GPG a un formato de texto encapsulado en ASCII, en lugar de binario
+gpg --list-secret-keys --keyid-format=long #Este comando se utiliza para listar las claves GPG secretas (privadas) disponibles en su llavero, mostrando sus ID en el formato completo y largo.
