078a9920c8db240194c9d423bf24479d0ac8b35a
Arialejo01 <arvelascogu@uide.edu.ec>
2026-03-27 03:07:17 +0000

    import friend key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -1,7 +1,7 @@
 uname -a #It provides information about the computer on which the code is running.
 which gpg # location of the gpg binary
 gpg --version #gives the version and additional information
-gpg --full-generate-key #I generate public and private keys
+gpg --full-generate-key gpg --list-secret-keys --keyid-format=long#I generate public and private keys
 gpg --list-keys #Displays all public GPG keys in your local keyring, including user IDs, key IDs, and fingerprints
 gpg --armor --export #Exports a GPG public key to an ASCII-encapsulated text format, instead of binary.
 gpg --list-secret-keys --keyid-format=long #This command is used to list the secret (private) GPG keys available in your keyring, showing their IDs in the full, long format.
