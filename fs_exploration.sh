70c7b299cc8b5511e0bbbdc4a675b0735d5d4d10
Arialejo01 <arvelascogu@uide.edu.ec>
2026-04-02 12:32:13 +0000

    list files cd

---
+++ fs_exploration.sh
@@ -0,0 +1,4 @@
+#Changes your current directory to the root directory (/), which is the very top level of the entire Linux file system hierarchy
+cd /
+#Lists the contents of the current directory, but adds a special character to the end of each name to indicate its file type. For example, it adds a / to directories and an * to executable files
+ls -F
