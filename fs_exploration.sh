fef0c12fee20b7083f8069c81bb67b9e8beba192
Arialejo01 <arvelascogu@uide.edu.ec>
2026-04-02 13:09:12 +0000

    parted  and lsblk

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -8,3 +8,9 @@ sudo apt update
 sudo apt upgrade
 #Install the GNU Parted tool on Debian/Ubuntu-based Linux systems
 sudo apt install parted
+#This command displays the system's disk and partition configuration. `sudo parted -l` lists storage devices and their partitions with details such as size, type, and labels. Then, `echo -e "\n---\n"` adds a separator line. Finally, `lsblk -f` displays the device tree with information about file systems, labels, UUIDs, and mounts. It is useful for diagnosing or verifying the storage status in Linux.
+sudo parted -l && echo -e "\n---\n" %% lsblk -f && echo -e "\n---\n"
+#It displays a detailed list of all available partitions and disks on the system, including their size, type, label, and file system.
+parted -l
+#It displays detailed information about block devices (disks, partitions, volumes) including their file system, label, UUID, and type.
+lsblk -f
