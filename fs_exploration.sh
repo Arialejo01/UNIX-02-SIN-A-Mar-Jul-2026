8bc3e8268e731f33c249ef68a84ffaf235a0ac8c
Arialejo01 <arvelascogu@uide.edu.ec>
2026-04-02 12:51:22 +0000

    sudo apt

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,9 @@
 cd /
 #Lists the contents of the current directory, but adds a special character to the end of each name to indicate its file type. For example, it adds a / to directories and an * to executable files
 ls -F
+#Updates the local package database (index) on Debian/Ubuntu-based Linux systems
+sudo apt update
+#Update all installed software packages to their latest versions on Debian/Ubuntu-based Linux systems
+sudo apt upgrade
+#Install the GNU Parted tool on Debian/Ubuntu-based Linux systems
+sudo apt install parted
