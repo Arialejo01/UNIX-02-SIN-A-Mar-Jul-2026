4c124e5ddc2984ee498d37d93dd1374540a2c9fd
Arialejo01 <arvelascogu@uide.edu.ec>
2026-04-09 12:25:39 +0000

    roots

---
+++ scrpit_boot.sh
@@ -0,0 +1,4 @@
+mkdir #Creates a new directory (though it requires a folder name to work)
+cd proyecto/ #Changes the current directory to the "proyecto" subdirectory
+cd .. #Moves the current directory up one level to the parent directory
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #Changes the current directory to the specific absolute path provided
