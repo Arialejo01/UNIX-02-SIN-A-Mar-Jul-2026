1e69dae88f95d464be8d64a23d2ff757a78e49b5
Arialejo01 <arvelascogu@uide.edu.ec>
2026-04-09 12:40:09 +0000

    cd

--- scrpit_boot.sh
+++ scrpit_boot.sh
@@ -2,3 +2,7 @@ mkdir #Creates a new directory (though it requires a folder name to work)
 cd proyecto/ #Changes the current directory to the "proyecto" subdirectory
 cd .. #Moves the current directory up one level to the parent directory
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #Changes the current directory to the specific absolute path provided
+cd #Changes the current directory to the current user's home directory
+cd ~ #Changes the current directory to the home directory using the tilde (~) shortcut
+cd $HOME #Changes the current directory to the home directory using the $HOME environment variable
+cd /home/codespace/ #Changes the current directory to the specific absolute path /home/codespace/
