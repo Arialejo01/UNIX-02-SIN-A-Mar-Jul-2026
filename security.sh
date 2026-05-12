56a2397f405104b7446c70789230afe80fa82fe4
Arialejo01 <arvelascogu@uide.edu.ec>
2026-05-12 17:00:14 +0000

    touch and mkdir

---
+++ security.sh
@@ -0,0 +1,17 @@
+
+    1  umask
+    2  touch archivo1
+    3  mkdir directorio1
+    4  ls -l
+    5  umask 027
+    6  touch archivo2
+    7  mkdir directorio2
+    8  ls -l
+   10  sudo apt upgrade
+   11  apt upgrade
+   12  apt update
+   13  apt install act
+   14  apt install acl
+   15  umask
+   16  chown -R $(whoami) .
+   17  setfacl -bnR .
