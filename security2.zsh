2e1180cd1006d009a1e8aa99192fbca82baa9590
Arialejo01 <arvelascogu@uide.edu.ec>
2026-05-28 12:59:49 +0000

    newgrp

--- security2.zsh
+++ security2.zsh
@@ -6,3 +6,10 @@ id -gn
 touch ~/test_grupo_heredado.txt
 #the group it's the principal group of teh user
 la -la ~/test_grupo_heredado.txt
+
+# View the current group
+id - gn
+echo "Grupo actual: $(id-gn)"
+# Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+Is -la ~/antes_de_newgrp.txt
