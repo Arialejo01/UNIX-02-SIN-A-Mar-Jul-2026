48014363b0ba2a2b7ae0b7139f72cede63c2ce6f
Arialejo01 <arvelascogu@uide.edu.ec>
2026-05-29 04:41:10 +0000

    level 13

--- script_linux_cisco_2.sh
+++ script_linux_cisco_2.sh
@@ -45,3 +45,43 @@ ls
 # Space Operations Computing for NASA. 
 # Curiosity, the Mars Rover, also uses it.
 
+# --- Level 13: Removing Files (rm) ---
+
+# The rm (remove) command is used to delete files and directories.
+# It is important to note that deleted files and directories do not go to a 
+# "trash bin" like they do in desktop-oriented operating systems.
+# When a file is deleted with the rm command, it is generally gone permanently.
+# Basic syntax: rm [OPTIONS] FILE
+
+# Change to the Documents directory
+cd ~/Documents
+
+# Without any options, the rm command is typically used to remove ordinary files
+rm linux.txt
+
+# Verify the deletion
+# This should return an error: "ls: cannot access linux.txt: No such file or directory"
+ls linux.txt
+
+# The rm command will ignore directories if you try to delete them without options
+# This will fail and return: "rm: cannot remove 'Work': Is a directory"
+rm Work
+
+# To delete a directory, you must use a recursive option, such as -r or -R.
+# Be careful, as these options are "recursive" and will delete the directory 
+# along with all of its files and subdirectories inside it.
+rm -r Work
+
+# Verify the directory deletion
+# This should return an error: "ls: cannot access Work: No such file or directory"
+ls Work
+
+# --- Warning ---
+# The rm command deletes files permanently. If you are practicing in a lab 
+# environment, you might need to reset the terminal to bring these files back.
+
+# --- Good to Know: Permissions ---
+# Permissions can affect file management commands, such as the rm command.
+# To delete a file inside a directory, the user must have write and execute 
+# permissions on that specific directory. Normally, ordinary users only have 
+# this type of permission in their home directory and its subdirectories.
