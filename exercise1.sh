05f184fb789fa698bd931bbc2b343bef26e2df3f
Arialejo01 <arvelascogu@uide.edu.ec>
2026-06-09 17:37:12 +0000

    documentation exercise1

--- exercise1.sh
+++ exercise1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FIRST_NAME="${1}"
-LAST_NAME="${2}"
-touch output.txt
-date "+%d-%m-%Y" > output.txt
-echo "${FIRST_NAME} ${LAST_NAME}" >> output.txt
-cp output.txt backup.txt
-cat output.txt 
+FIRST_NAME="${1}" # Assigns the first positional argument to a variable
+LAST_NAME="${2}" # Assigns the second positional argument to a variable
+touch output.txt # Creates a new empty file named output.txt
+date "+%d-%m-%Y" > output.txt # Writes the formatted date, overwriting the file's content
+echo "${FIRST_NAME} ${LAST_NAME}" >> output.txt # Appends the full name to the end of the file
+cp output.txt backup.txt # Copies output.txt to create a backup file
+cat output.txt # Reads and prints the file's content to the standard output
