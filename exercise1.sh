8faa1eb2dbebeae5bf1ce03df0cdfaf1f98d2e8a
Arialejo01 <arvelascogu@uide.edu.ec>
2026-06-09 17:31:24 +0000

    exercise1

---
+++ exercise1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FIRST_NAME="${1}"
+LAST_NAME="${2}"
+touch output.txt
+date "+%d-%m-%Y" > output.txt
+echo "${FIRST_NAME} ${LAST_NAME}" >> output.txt
+cp output.txt backup.txt
+cat output.txt 
